1450c633ab1302ccc4cebea3ac3a3461473e9b9d
Pingua-cyber <roshanpingua123@gmail.com>
2025-09-19 23:44:02 +0530

    2nd commit

---
+++ Ques_6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    char ch;
+    std::cout << "Enter a character: ";
+    std::cin >> ch;
+    std::cout << "ASCII code: " << (int)ch << std::endl;
+    return 0;
+}
+
